ad849bb18ffe7b6073f3306d3b94c10fe05608ec
Florian Lehmann <florian.lehmann@uni-bayreuth.de>
2024-02-08 17:08:36 +0100

    prevent overscroll on root html element

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 export const preventOverscrollRefresh = () => {
-    if (!document.body.classList.contains('prevent-scroll-refresh')) {
+    if (!document.body.classList.contains('prevent-scroll-refresh') || document.documentElement.classList.contains('prevent-scroll-refresh')) {
+        document.documentElement.classList.add('prevent-scroll-refresh');
         document.body.classList.add('prevent-scroll-refresh');
     }
 }
